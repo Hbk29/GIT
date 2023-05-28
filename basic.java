569d154e6ad63b92836c4f09296eba54b7502901
Hbk29 <129576120+Hbk29@users.noreply.github.com>
2023-05-28 11:08:29 +0530

    adding files

---
+++ basic.java
@@ -0,0 +1,62 @@
+package BDDCucumber;
+
+public class basic {
+	/* BDD ===ITS FRAMEWORK Behaviour driven development
+	 * Cucumber===itd testing approch which implement
+	 * BDD
+	 * 
+	 * iT EXPLAIND BEHAVIOUR OF APP USING GHERKIN LANGUAGE
+	 * 
+	 * In BDD, we write testcases using Gherkin language by using Given, When, Then keywords
+	 * so can anyone can understand it.
+	 * We use Given, When, Then keyord.
+	 * AND,But these keywords are extension of GIven,When,Then 
+	 * 
+	 * Given====pre condition
+	 * When====Event
+	 * Then====Expected output
+	 * 
+	 * ex.
+	 * Scenerio==Log in facebook page
+	 * @tag==log in
+	 * 
+	 * Given=====facebook url is open.
+	 * When====When user enters username.
+	 * And====user enters password.
+	 * And====click on login button.
+	 * Then====facebook log in page opens.
+	 * 
+	 * BDD cucumber required 4 files
+	 * 1.feature file====write TestCases using gherkin lang 
+	 * 2.Runner class==== it provides info of featutre==path of feature file, glue==package name, @tag,dry run==false(check only present Tc), monochrome===true(Beutify) 
+	 * 3.Step defn file====
+	 * 4.page class====
+	 * 
+	 *only feature file has .feature extension other 3 files have. java extension
+	 * 
+	 * Steps====
+	 * ADD JARS
+	 * ADD CUCUMBER COMPILER
+	 * 1.create feature file having .feature file
+	 * 2.create runner class file===(pass the path, package name, tag tec)
+	 * 3.Run the feature file
+	 * 4.copy paste console date
+	 * 5.create step def file====paste the copied data
+	 * 6.create page class file
+	 * 7.define data with webelement
+	 * 8. create object of page class
+	 *  in step def file and
+	 * call method by help of ref of object
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
