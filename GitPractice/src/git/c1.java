b5925f72ea264c2939196b32d2986e7aeb7eea5b
umesh p <ussp29@gmail.com>
2023-05-28 10:45:28 +0530

    my first share

---
+++ GitPractice/src/git/c1.java
@@ -0,0 +1,25 @@
+package git;
+
+public class c1 {
+	public static void main(String[] args) {
+		
+		String a="hbk vs HHH";
+		
+		int length=a.length();
+		System.out.println(length);
+		
+		char e=a.charAt(5);
+		
+		System.out.println(e);
+		
+		System.out.println(a.toLowerCase());
+		System.out.println(a.toUpperCase());
+		
+		boolean b=a.equals("hbk vs hhH");
+		
+		System.out.println(b);
+		
+		System.out.println(a.equalsIgnoreCase("hbk vs HHH"));
+	}
+	
+}
